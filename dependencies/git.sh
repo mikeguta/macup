530b3d6decbbade06b46fd808a6c01e58b2c71d1
mikeguta <7657388+mikeguta@users.noreply.github.com>
2023-11-22 17:18:41 +0000

    fix: robust git config and no global gitignore

--- dependencies/git.sh
+++ dependencies/git.sh
@@ -5,13 +5,29 @@
 . $(pwd)/helpers/print.sh
 
 #
-# GIT_USERNAME and GIT_EMAIL should be declared in env
+# GIT_USERNAME and GIT_EMAIL can be declared in env
 #
-create_git_config() {
-    cp $1 $HOME/.gitconfig
-    print_success "Git config created"
-    cp $2 $HOME/.gitignore
-    print_success "Git ignore created"
+configure_git_user() {
+
+    # If there is an existing username, we read that
+    if [ -z "${GIT_USERNAME}" ]; then
+        GIT_USERNAME=$(git config --global --get user.name)
+    fi
+    if [ -z "${GIT_EMAIL}" ]; then
+        GIT_EMAIL=$(git config --global --get user.email)
+    fi
+    
+    if [ -z "${GIT_USERNAME}" ]; then
+        print_question "Git username"
+        read GIT_USERNAME
+    fi
+    export GIT_USERNAME
+
+    if [ -z "${GIT_EMAIL}" ]; then
+        print_question "Git email"
+        read GIT_EMAIL
+    fi
+    export GIT_EMAIL
 
     print_info "username: ${GIT_USERNAME}"
     git config --global user.name $GIT_USERNAME
@@ -19,18 +35,31 @@ create_git_config() {
     print_info "email: ${GIT_EMAIL}"
     git config --global user.email $GIT_EMAIL
 
-    print_info "gitignore: ${HOME}/.gitignore"
-    git config --global core.excludesfile $HOME/.gitignore
+    print_success "Git: user, email set"
+}
 
-    print_success "Git: user, email and gitignore added"
+
+configure_git_colors() {
+    git config --global "color.ui" "auto"
+    git config --global "color.branch.current" "yellow bold"
+    git config --global "color.branch.local" "green bold"
+    git config --global "color.branch.remote" "cyan bold"
+    git config --global "color.diff.meta" "yellow bold"
+    git config --global "color.diff.frag" "magenta bold"
+    git config --global "color.diff.old" "red bold"
+    git config --global "color.diff.new" "green bold"
+    git config --global "color.diff.whitespace" "red reverse"
+    git config --global "color.status.added" "green bold"
+    git config --global "color.status.changed" "yellow bold"
+    git config --global "color.status.untracked" "red bold"
+
+    print_success "Git: colors configured"
 }
 
 configure_git(){
-    if ask "Would you configure git? ${1}" Y; then
-        create_backup "git" "gitconfig" $HOME/.gitconfig
-        create_backup "git" "gitignore" $HOME/.gitignore
-
-        create_git_config $1 $2
+    if ask "Would you like to configure git?" Y; then
+        configure_git_colors
+        configure_git_user
     else
         print_error "Aborting..."
     fi
--- utils/prompt.sh
+++ utils/prompt.sh
@@ -6,26 +6,15 @@
 . $(pwd)/utils/styles.sh
 
 prompt() {
-    if ask "UP will configure this machine and requires Admin access. Are you sure?" Y; then
+    if ask "MacUp will configure this machine and requires Admin access. Are you sure?" Y; then
         # Override for development
         if ! [ "${UP_ENV}" = "development" ]; then
             admin_access
         fi
 
         else
-        print_error "UP requires 'Admin Access' to be granted; Exiting now. \n"
+        print_error "MacUp requires 'Admin Access' to be granted; Exiting now. \n"
         exit 0
     fi;
 
-    if [ -z "${GIT_USERNAME}" ]; then
-        print_question "Git username"
-        read GIT_USERNAME
-        export GIT_USERNAME
-    fi
-
-    if [ -z "${GIT_EMAIL}" ]; then
-        print_question "Git email"
-        read GIT_EMAIL
-        export GIT_EMAIL
-    fi
 }
