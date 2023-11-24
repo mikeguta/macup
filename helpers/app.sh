d76cda0230f370413d415571063e35e532ba45c1
mikeguta <7657388+mikeguta@users.noreply.github.com>
2023-11-24 16:40:19 +0000

    feat: app_is_installed helper

---
+++ helpers/app.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+. $(pwd)/helpers/print.sh
+
+is_app_installed() {
+    if [ -z "${1}" ]; then
+        print_error "Application name was not specified."
+        exit 2;
+    fi
+
+    local app_name="$1.app"
+    mdfind -onlyin /Applications "kMDItemKind == 'Application'" | grep "/$app_name$" > /dev/null
+}
