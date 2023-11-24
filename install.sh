4305cf35f0f6cf8ef45afe47acfcc1f5ee312dc2
mikeguta <7657388+mikeguta@users.noreply.github.com>
2023-11-24 14:04:46 +0000

    fix: paths renamed to macup

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@
 #
 GITHUB_SHA=${1:-master}
 
-echo "\n UP Github SHA: ${GITHUB_SHA} \n"
+echo "\n MacUp Github SHA: ${GITHUB_SHA} \n"
 
-mkdir -p ~/.up; cd ~/.up; \
+mkdir -p ~/.macup; cd ~/.macup; \
 curl -L "https://github.com/mikeguta/macup/tarball/${GITHUB_SHA}" | tar -xzv --strip-components 1 &> /dev/null; \
 sh bootstrap.sh
