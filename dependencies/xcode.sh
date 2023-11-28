a7b810daba83e6c6272a7b8dd3b8da503d526f0b
mikeguta <7657388+mikeguta@users.noreply.github.com>
2023-11-28 14:14:43 +0000

    fix: remove stray stdout message

--- dependencies/xcode.sh
+++ dependencies/xcode.sh
@@ -3,10 +3,10 @@
 . $(pwd)/helpers/print.sh
 
 install_xcode_clt() {
-    if type xcode-select >&- && xpath=$( xcode-select --print-path ) &&
+    if type xcode-select &> /dev/null && xpath=$( xcode-select --print-path ) &&
         test -d "${xpath}" && test -x "${xpath}" ; then
         print_success "Xcode already installed."
-        else
+    else
         xcode-select --install
         print_success "Please install xcode and re-run MacUp!"
         exit 1
